e84dfac94d53f00052d32236ef1e03e4a1edaba4
Elyas Mir <said_i@auca.kg>
2020-10-05 06:24:44 +0600

    Finish problem 1.4 form homework

---
+++ lab-01/src/Problem1_4.java
@@ -0,0 +1,11 @@
+public class Problem1_4 {
+    public static void main(String[] args) {
+        System.out.println("a\t\ta^2\t\ta^3");
+        for (int a=1; a <= 4 ; a++){
+            System.out.println(a+"\t\t"+(int)Math.pow(a,2)+"\t\t"+(int)Math.pow(a,3));
+
+
+        }
+
+    }
+}

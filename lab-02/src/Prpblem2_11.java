c89a15ebf806495df710167dc3f6419f155628d1
Elyas Mir <said_i@auca.kg>
2020-10-22 11:33:01 +0600

    finish problem 2.11 lab 2 homework

--- lab-02/src/Prpblem2_11.java
+++ lab-02/src/Prpblem2_11.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Prpblem2_11 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print(" Enter the number of years: ");
+        int year = scan.nextInt();
+
+        double initial_population =312032486;
+
+        double second_in_one_year = 60 * 60 * 24 * 365;
+
+        double total_birth_in_one_year = second_in_one_year / 7.0;
+        double total_death_in_one_year = second_in_one_year / 13.0;
+        double total_migrant_in_one_year = second_in_one_year / 45.0;
+
+        double new_population_range = (total_birth_in_one_year + total_migrant_in_one_year)-total_death_in_one_year;
+
+        for (int x = 0 ; x < year; x++){
+            initial_population += new_population_range;
+
+        }
+        System.out.printf("The population in %d years is %.0f ",year, initial_population);
 
     }
 }

18ae8d036e123ea4402c279b7c39707fdfad5e4f
Elyas Mir <said_i@auca.kg>
2020-10-22 19:19:11 +0600

    finish problem 02 lab 3

--- lab-03/src/Problem02.java
+++ lab-03/src/Problem02.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Problem02 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Number of points? ");
+        int point = scan.nextInt();
+
+        if (point >= 40 ){
+            System.out.println("You passed “Structured Programming”!!!\n" +
+                    "You should take “Object-Oriented Programming” in the next semester ");
+        }
+        else {
+            System.out.println("You failed “Structured Programming”!!!\n" +
+                    "You can take it again in the fall semester of 2021.");
+        }
+    }
 }

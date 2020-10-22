5f4d23c7bbb205104b3861f7ff516c0be5752858
Elyas Mir <said_i@auca.kg>
2020-10-22 19:32:56 +0600

    finish problem 03 lab 3

--- lab-03/src/Problem03.java
+++ lab-03/src/Problem03.java
@@ -4,14 +4,31 @@ public class Problem03 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Year? ");
-        int year = scan.nextInt();
+        System.out.print("1st number? ");
+        int num1 = scan.nextInt();
 
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ){
-            System.out.println("This is a leap year.");
+        System.out.print("2nd number? ");
+        int num2 = scan.nextInt();
+
+        System.out.print("3rd number? ");
+        int num3 = scan.nextInt();
+
+        int max = 0;
+
+        if (num1 > num2){
+            if(num1 > num3){
+                max = num1;
+            }else {
+                max = num3;
+            }
         }else {
-            System.out.println("This is not a leap year.");
+            if (num2 > num3){
+                max = num2;
+            } else {
+                max = num3;
+            }
         }
 
+        System.out.printf("The value %d is the greatest one.%n",max);
     }
 }

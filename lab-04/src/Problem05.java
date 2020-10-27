19d159bbda01fc0048e0d70503f31c51c1b02b3f
Elyas Mir <said_i@auca.kg>
2020-10-27 15:20:13 +0600

    finish Problem 05 lab 4

---
+++ lab-04/src/Problem05.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Problem05 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Year? ");
+        int year = scan.nextInt();
+
+        System.out.println("Month? ");
+        int month = scan.nextInt();
+
+        int days = 0;
+
+        switch (month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                days = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+                days = 30;
+                break;
+
+            case 2:
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ){
+                    days = 29;
+                }else {
+                    days = 28;
+                }
+                    break;
+            default:
+                System.out.println("Invalid month number ");
+                break;
+        }
+
+        System.out.println("Number of days: " + days );
+    }
+}

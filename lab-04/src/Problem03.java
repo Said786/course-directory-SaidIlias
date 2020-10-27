9f0f95482c95ac9368c11fbc96b024387beef531
Elyas Mir <said_i@auca.kg>
2020-10-27 15:02:56 +0600

    finish Problem 03 lab 4

---
+++ lab-04/src/Problem03.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the month number: ");
+        int month =  scan.nextInt();
+
+        switch (month){
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Winter");break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Fall");break;
+            default:
+                System.out.println("Invalid month number");
+                break;
+        }
+
+    }
+}
